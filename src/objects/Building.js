be013e00b3e8156673e30dee9da59c45fbd34e47
Vladimir Goncharov <proteyrus@gmail.com>
2018-05-03 20:14:38 +0300

    Add building support

--- src/objects/Building.js
+++ src/objects/Building.js
@@ -1,10 +1,36 @@
 import { Object3D } from '../core/Object3D.js';
+import { MeshBasicMaterial } from "../materials/MeshBasicMaterial";
+import { TrianglesDrawMode } from "../constants";
+import { BoxBufferGeometry } from "../geometries/BoxGeometry";
 
-function Building() {
+function Building( width, height, depth, levels ) {
 
 	Object3D.call( this );
 
 	this.type = 'Building';
+
+	this.parameters = {
+		width: width,
+		height: height,
+		depth: depth,
+		widthSegments: 1,
+		heightSegments: levels,
+		depthSegments: 1
+	};
+
+	this.parameters.heightSegments = Math.floor( heightSegments ) || 1;
+
+
+	this.geometry = new BoxBufferGeometry(
+		this.parameters.width,
+		this.parameters.height,
+		this.parameters.depth,
+		this.parameters.widthSegments,
+		this.parameters.heightSegments,
+		this.parameters.depthSegments );
+	this.material = new MeshBasicMaterial( { color: Math.random() * 0xffffff } );
+
+	this.drawMode = TrianglesDrawMode;
 
 }
 
@@ -15,3 +41,5 @@ Building.prototype = Object.assign( Object.create( Object3D.prototype ), {
 	isBuilding: true
 
 } );
+
+export { Building };
